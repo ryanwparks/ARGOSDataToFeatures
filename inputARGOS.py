489c96d4f49467b684e3213b43c8ecd22a4e5ffb
ryanwparks <42747210+ryanwparks@users.noreply.github.com>
2018-10-15 12:12:53 -0400

    Imported modules

---
+++ inputARGOS.py
@@ -0,0 +1,14 @@
+##---------------------------------------------------------------------
+## ImportARGOS.py
+##
+## Description: Read in ARGOS formatted tracking data and create a line
+##    feature class from the [filtered] tracking points
+##
+## Usage: ImportArgos <ARGOS folder> <Output feature class> 
+##
+## Created: Fall 2018
+## Author: ryan.parks@duke.edu (for ENV859)
+##----------------------------------------------------------------
+
+# Import modules
+import sys, os, arcpy
